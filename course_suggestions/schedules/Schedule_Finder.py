509f1a63051b2aacf510d4a8d44775ba661210df
rmallela26 <rishabh.mallela@yahoo.com>
2024-06-06 13:38:49 -0700

    added build trees, find neighbors, and distance functions

--- course_suggestions/schedules/Schedule_Finder.py
+++ course_suggestions/schedules/Schedule_Finder.py
@@ -4,24 +4,43 @@
 
 from sklearn.neighbors import KDTree
 import numpy as np
+from .models import Schedule
 
 class Schedule_Finder:
     def __init__(self, majors):
         self.trees = {} #holds the trees for different majors
         self.majors = majors
 
+        # self.build_trees()
+
     #custom distance function between two points
     def distance(x, y):
-        #this is distance within one major
-        #[majors (if more than one, otherwise 00), term, minors, ]
-        return x-y
-    
-    # vectorize everything in the database
-    def vectorize(self):
-        return
+        #this is distance within one major. Everything is already numbers
+        # [major, minor, term, career, special things (entrepreneurship, phd), summer, concentration (specific)]
 
-    def build_trees(self, arr):     
-        kd_tree = KDTree(arr, metric=self.distance)
-        nearest_neighbor = []
-        return nearest_neighbor
+        #[major, minor, concentration, term, career, special, summer, something specific you want to learn]
+        weights = [3, 7, 3, 7, 2, 2, 1, 2]
+        distance = 0
+
+        for i in range(len(x)):
+            if i == 3: #if it is the term 
+                distance += weights[i] * abs(x[i] - y[i])
+            else:
+                if x[i] != y[i]: 
+                    distance += weights[i]
+
+        return distance 
     
+    def build_trees(self):    
+        for major in self.majors:
+            #get schedules 
+            schedules = Schedule.objects.filter(major__contains=major)
+
+            kd_tree = KDTree(schedules, metric=self.distance)
+            
+            #add tree to trees dict
+            self.trees[major] = kd_tree
+    
+    def getNearestNeighbors(self, num_neighbors, major, query_schedule):
+        distances, indices = self.trees[major].query([query_schedule], k=num_neighbors)
+        return indices, distances
--- course_suggestions/schedules/apps.py
+++ course_suggestions/schedules/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
+from .Schedule_Finder import Schedule_Finder
 
+scheduler = None
 
 class SchedulesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'schedules'
+    def ready(self):
+        print("Hello World from apps.py")
+        print()
+        print()
+
+        global scheduler
+        scheduler = Schedule_Finder(['computer science', 'electrical engineering'])
